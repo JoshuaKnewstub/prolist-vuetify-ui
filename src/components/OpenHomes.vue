3096e263ed5d07cf168d50efbc2f0ed800c586e8
JamesRaymer <james.raymer@my.jcu.edu.au>
2020-12-14 14:35:11 +1000

    Add OpenHomes Grid layout

---
+++ src/components/OpenHomes.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="openHomes">
+    <h1 class="openHomesHeading">Open Homes</h1>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+        <v-flex xs12 md6 lg4>
+          <v-btn outline block class="primary">Test</v-btn>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'OpenHomes',
+
+    data: () => ({
+    }),
+  }
+</script>
+<style scoped>
+  .openHomesHeading {
+    text-align: center;
+  }
+</style>
