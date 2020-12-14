9f7ea6941c6e0063e8750448aa5b064314afa92c
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-14 13:58:08 +1000

    add NavBar layout
    add a toolbar for page information, a side nav for page selection and buttons for filtering searchs

---
+++ src/components/layout/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+<nav>
+    <v-toolbar app>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title class="text-uppercase grey--text">
+            {{ pageTitle }}
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat color="gray">
+            <span>Search</span>
+            <v-icon right>mdi-magnify</v-icon>
+        </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer app v-model="drawer" >
+     <v-list
+          nav
+          dense
+        >
+          <v-list-item-group
+            v-model="group">
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-36px mdi-home-group</v-icon></v-list-item-icon>
+              <v-list-item-title>Open Houses</v-list-item-title>
+            </v-list-item>
+  
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-36px mdi-home-heart</v-icon></v-list-item-icon>
+              <v-list-item-title>Favourites</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon><v-icon>mdi-36px mdi-home-map-marker</v-icon></v-list-item-icon>
+              <v-list-item-title>Visited</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+    </v-navigation-drawer>
+</nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            drawer: true,
+            pageTitle: "Open Houses"
+        }
+    }
+}
+</script>
