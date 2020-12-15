42ab3217ea1cf54ee1b3fc4aca0800e941f32cc6
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-15 10:45:14 +1000

    add JSon file reader

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-main>
       <NavBar/>
       <OpenHomes/>
+      <Listings/>
     </v-main>
   </v-app>
 </template>
@@ -10,6 +11,7 @@
 <script>
 import OpenHomes from './components/OpenHomes';
 import NavBar from './components/layout/NavBar'
+import Listings from './components/listings/Listings'
 
 export default {
   name: 'App',
@@ -17,6 +19,7 @@ export default {
   components: {
     OpenHomes,
     NavBar,
+    Listings
   },
 
   data: () => ({
---
+++ src/components/listings/Listing.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="listing">
+        <ul>
+            <li v-for="data in propertyData" :key="data.id">
+                {{data.PropertyType}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import listingsJson from "./components/listings/Listings.JSON";
+
+export default {
+  data() {
+    return {
+      propertyData: listingsJson,
+    };
+  },
+};
+</script>
