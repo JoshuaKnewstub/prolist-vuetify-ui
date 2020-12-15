bc1b38e9badb3ca42907768365a5c21af2afa8b0
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-15 15:23:15 +1000

    Revert "install axios"
    This reverts commit 29495183ff74c8880c801c911c25a4ff132053e7.

--- src/components/listings/Listing.vue
+++ src/components/listings/Listing.vue
@@ -1,18 +1,19 @@
 <template>
-  <div id="listing">
-    {{ data }}
-  </div>
+    <div id="listing">
+        <ul>
+            <li v-for="data in propertyData" :key="data.id">
+                {{data.PropertyType}}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+
 export default {
   data() {
-    return {};
-  },
-  mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.data = response));
+    return {
+    };
   },
 };
 </script>
