80810a1287aee619d83f8070bbc3c24e76cad715
JamesRaymer <james.raymer@my.jcu.edu.au>
2020-12-15 10:20:56 +1000

    Revert "Add card template"
    This reverts commit 4a5d2beebfafd66f52e9e11ae9a15974d3ade1cb.

--- src/components/OpenHomes.vue
+++ src/components/OpenHomes.vue
@@ -1,42 +1,43 @@
 <template>
   <div class="openHomes">
     <h1 class="openHomesHeading">Open Homes</h1>
-    <v-layout row wrap>
-      <v-card class="mx-auto" id="homeCard" max-width="344" wrap row>
-        <v-img
-          height="200px"
-        >Image Placeholder</v-img>
+    <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      height="200px"
+    >Image Placeholder</v-img>
 
-        <v-card-title class="address">
-          Address of House
-        </v-card-title>
+    <v-card-title class="address">
+      Address of House
+    </v-card-title>
 
-        <v-card-subtitle class="suburb">
-          Suburb of House
-        </v-card-subtitle>
+    <v-card-subtitle class="suburb">
+      Suburb of House
+    </v-card-subtitle>
 
-        <v-card-title class="pricing">
-          Pricing of House
-        </v-card-title>
-        <v-card-actions>
-          <v-btn
-            color="blue lighten-2"
-            text
-          >
-            Explore
-          </v-btn>
-          <v-spacer></v-spacer>
-            <v-card-text>0</v-card-text>
-            <v-icon>mdi-36px mdi-bed</v-icon>
-          <v-spacer></v-spacer>
-            <v-card-text>0</v-card-text>
-            <v-icon>mdi-36px mdi-shower</v-icon>
-          <v-spacer></v-spacer>
-            <v-card-text>0</v-card-text>
-            <v-icon>mdi-36px mdi-car</v-icon>
-        </v-card-actions>
-      </v-card>
-    </v-layout>
+    <v-card-title class="pricing">
+      Pricing of House
+    </v-card-title>
+    <v-card-actions>
+      <v-btn
+        color="blue lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+      <v-spacer></v-spacer>
+        <v-card-text>0</v-card-text>
+        <v-icon>mdi-36px mdi-bed</v-icon>
+      <v-spacer></v-spacer>
+        <v-card-text>0</v-card-text>
+        <v-icon>mdi-36px mdi-shower</v-icon>
+      <v-spacer></v-spacer>
+        <v-card-text>0</v-card-text>
+        <v-icon>mdi-36px mdi-car</v-icon>
+    </v-card-actions>
+  </v-card>
   </div>
 </template>
 
@@ -52,9 +53,5 @@
 <style scoped>
   .openHomesHeading {
     text-align: center;
-    padding: 5%;
-  }
-  #homeCard {
-    margin: 20px;
   }
 </style>
