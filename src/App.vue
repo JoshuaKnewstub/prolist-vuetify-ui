b0b338400990f58c4f4bee1c5805817a30c93932
JamesRaymer <james.raymer@my.jcu.edu.au>
2020-12-16 10:12:06 +1000

    Import ListingPreview instead of Listing

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-main>
       <NavBar/>
       <OpenHomes/>
-      <Listing/>
+      <ListingPreview/>
     </v-main>
   </v-app>
 </template>
@@ -11,7 +11,7 @@
 <script>
 import OpenHomes from './components/OpenHomes.vue';
 import NavBar from './components/layout/NavBar.vue'
-import Listing from './components/Listing.vue'
+import ListingPreview from './components/ListingPreview.vue'
 
 export default {
   name: 'App',
@@ -19,7 +19,7 @@ export default {
   components: {
     OpenHomes,
     NavBar,
-    Listing
+    ListingPreview
   },
   data: () => ({    
   }),
