3785a3960623505aaa4677a55cf7d463b3190e77
Joshua Knewstub <36719680+JoshuaKnewstub@users.noreply.github.com>
2020-12-17 13:49:28 +1000

    update explorehome to get listing from api

--- src/components/ExploreHome.vue
+++ src/components/ExploreHome.vue
@@ -1,19 +1,46 @@
 <template>
-    <div>
-        <h1>Test</h1>
-        
-    </div>
+  <div>
+    <h1>Test</h1>
+    <h1 v-if="response"> {{ listing }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    mounted:function(){
-        console.log(this.$route.params.listingid)
-    }
+    responce: null,
 
+    data: () => ({
+        response: null,
+    }),
+
+    mounted:function(){
+        const listingId = this.$route.params.listingid
+        console.log(listingId)
+
+        const headers = new Headers();
+    headers.append("x-prolist-client-website-id","1e4f890d-6112-45fe-ba82-c67bc30116a1");
+    headers.append("x-prolist-website-id","f94cda42-f8b6-48e3-850a-aef60b3cfc96");
+    headers.append("x-prolist-website-level", "3");
+
+    const request = new Request(
+      "https://clientapi.prolist.net.au/api/listings/" + listingId,
+      {
+        headers,
+        mode: "cors",
+        cache: "default",
+      }
+    );
+    fetch(request)
+      .then((response) => response.json())
+      .then((listing) => {
+        console.log(listing);
+        this.response = listing;
+      });
+  },
+    
 }
+
 </script>
 
 <style scoped>
-
 </style>
